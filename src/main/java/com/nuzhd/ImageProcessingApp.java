a59c014cba4599978a1f3be0d05e0ec2d15a971d
Нуждин Дмитрий Алексеевич <DNuzhdin@inno.local>
2025-10-27 20:39:29 +0300

    nothing

--- src/main/java/com/nuzhd/ImageProcessingApp.java
+++ src/main/java/com/nuzhd/ImageProcessingApp.java
@@ -1,6 +1,5 @@
 package com.nuzhd;
 
-import io.minio.MinioClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,11 +8,4 @@ public class ImageProcessingApp {
     public static void main(String[] args) {
         SpringApplication.run(ImageProcessingApp.class, args);
     }
-
-    private final MinioClient minioClient;
-
-    public ImageProcessingApp(MinioClient minioClient) {
-        this.minioClient = minioClient;
-    }
-
 }
